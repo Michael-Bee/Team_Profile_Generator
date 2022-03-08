8950c89f737e5897fc7b5cdf0014f262c50d2e01
Michael Bee <bee.michael.02@gmail.com>
2022-03-07 20:39:19 -0600

    Started template HTML

--- index.js
+++ index.js
@@ -1,67 +1,74 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
+
+//HTML TEMPLATE
 const generateHTML = ({ name, location, github, linkedin }) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${name}</h1>
-    <p class="lead">I am from ${location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${github}</li>
-      <li class="list-group-item">LinkedIn: ${linkedin}</li>
-    </ul>
-  </div>
-</div>
+`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+        <title>Team Profile Generator</title>
+    </head>
+    <body>
+        <div class="container mw-100  bg-danger">
+            <h1 class="p-5 text-white">My Team</h1> 
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <div class="card shadow-sm" style="width:20rem;">
+                        <div class="card-header bg-primary p-3">
+                            <h5 class="card-title text-white">Employee Name</h5>
+                            <h5 class="card-title text-white"> Logo / Employee Role</h5>
+                        /div>
+                        <div class="card-body p-2 bg-light">
+                            <p class="card-text m-1 p-1 bg-white border border-secondary">Employee ID: ${employee.id}</p>
+                            <p class="card-text m-1 p-1 bg-white border border-secondary">Email: ${employee.email}</p>
+                            <p class="card-text m-1 p-1 bg-white border border-secondary">Office/GitHub/School: ${engineer.github}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>`;
 
+
+//USER PROMPTS
 inquirer
-  .prompt([
+    .prompt([
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+        type: 'input',
+        name: 'name',
+        message: `What is your manager's name?`,
     },
     {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+        type: 'input',
+        name: 'id',
+        message: 'What is their employee ID number?',
     },
     {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
+        type: 'input',
+        name: 'email',
+        message: 'What is their email address?',
     },
     {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+        type: 'input',
+        name: 'office',
+        message: 'What is their office number?',
     },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
-    },
-    {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
-    },
-  ])
-  .then((answers) => {
+    ])
+    .then((answers) => {
     const htmlPageContent = generateHTML(answers);
 
+//WRITES HTML FILE
     fs.writeFile('index.html', htmlPageContent, (err) =>
-      err ? console.log(err) : console.log('Successfully created index.html!')
+        err ? console.log(err) : console.log('Successfully created index.html!')
     );
-  });
+    });
