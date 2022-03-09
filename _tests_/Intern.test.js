6d8c4eedfc308f29b8aa6e3a39bac6ae0296c6e1
Michael Bee <bee.michael.02@gmail.com>
2022-03-08 21:27:58 -0600

    Finished Intern.test.js

---
+++ _tests_/Intern.test.js
@@ -0,0 +1,33 @@
+const Intern = require("../lib/Intern");
+
+describe("THIS IS TEST FOR INTERN CLASS ", ()=> {
+    it("should have a school property when instantiated with the school parameter", ()=> {
+        const name = "Mike";
+        const id = 02;
+        const email = "Mike@mail.com";
+        const school = "University of Denver";
+
+        const emp = new Intern(name, id, email, school);
+        expect(emp.school).toEqual(school);
+    })
+
+    it("Should return the github property when getGithub method is invoked", ()=> {
+        const name = "Mike";
+        const id = 02;
+        const email = "Mike@mail.com";
+        const school = "University of Denver";
+
+        const emp = new Intern(name, id, email, school);
+        expect(emp.getSchool()).toEqual(school);
+    })
+
+    it("Should return 'Engineer' when the getRole method is invoked", ()=> {
+        const name = "Mike";
+        const id = 02;
+        const email = "Mike@mail.com";
+        const school = "University of Denver";
+
+        const emp = new Intern(name, id, email, school);
+        expect(emp.getRole()).toEqual("Intern");
+    })
+})
