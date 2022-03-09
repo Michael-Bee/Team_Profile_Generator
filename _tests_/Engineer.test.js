f76456b332e79a69e29f383615b00ebb4278b34b
Michael Bee <bee.michael.02@gmail.com>
2022-03-08 21:24:33 -0600

    Finished Engineer.test.js

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,8 +1,33 @@
-const Employee = require("../lib/Engineer")
+const Engineer = require("../lib/Engineer");
 
-it('Should setup a github property when instantiated with a github parameter', () => {
-    const github = "Mike-Bee"
-    const testEmp = new Employee(github);
+describe("THIS IS TEST FOR ENGINEER CLASS ", ()=> {
+    it("should have a github property when instantiated with the github parameter", ()=> {
+        const name = "Mike";
+        const id = 02;
+        const email = "Mike@mail.com";
+        const github = "Michael-Bee";
 
-    expect(testEmp.github).toEqual("Mike-Bee")
-}) 
+        const emp = new Engineer(name, id, email, github);
+        expect(emp.github).toEqual(github);
+    })
+
+    it("Should return the github property when getGithub method is invoked", ()=> {
+        const name = "Mike";
+        const id = 02;
+        const email = "Mike@mail.com";
+        const github = "Michael-Bee";
+
+        const emp = new Engineer(name, id, email, github);
+        expect(emp.getGithub()).toEqual(github);
+    })
+
+    it("Should return 'Engineer' when the getRole method is invoked", ()=> {
+        const name = "Mike";
+        const id = 02;
+        const email = "Mike@mail.com";
+        const github = "Michael-Bee";
+
+        const emp = new Engineer(name, id, email, github);
+        expect(emp.getRole()).toEqual("Engineer");
+    })
+})
